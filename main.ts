3a4da2eb2ca3721d2679790310108ae3b8b3a230
M Williams <markjw@truro-penwith.ac.uk>
2021-10-29 14:26:57 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,81 @@
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Fire1, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    radio.sendString("f1")
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Fire2, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    radio.sendString("f2")
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Down, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    radio.sendString("down")
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Up, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    radio.sendString("up")
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "up") {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+    } else if (receivedString == "down") {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    } else if (receivedString == "right") {
+        basic.showLeds(`
+            . . # . .
+            . . # # .
+            # # # # #
+            . . # # .
+            . . # . .
+            `)
+    } else if (receivedString == "left") {
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            # # # # #
+            . # # . .
+            . . # . .
+            `)
+    } else if (receivedString == "f1") {
+        basic.showLeds(`
+            . # # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . # # # .
+            `)
+    } else if (receivedString == "f2") {
+        basic.showLeds(`
+            # # # # #
+            . . . . #
+            . . . # .
+            . . # . .
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Right, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    radio.sendString("right")
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Left, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    radio.sendString("left")
+})
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
